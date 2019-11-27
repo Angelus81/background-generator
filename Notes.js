0a939c44b684cd585f372872d3b6cff7e353c23d
dee <57253652+Angelus81@users.noreply.github.com>
2019-11-27 20:18:08 +0200

    updated notes.

--- Notes.js
+++ Notes.js
@@ -1,14 +1,14 @@
-To check version of Node:
+// To check version of Node:
 npm -v
 5.5.1
 
 node -v
 v12.13.1
 
-To upgrade, run: 
+// To upgrade, run: 
 npm install npm@latest -g
 
-Creates package.json file:
+// Creates package.json file:
 npm init
 This utility will walk you through creating a package.json file.
 It only covers the most common items, and tries to guess sensible defaults.
@@ -21,13 +21,13 @@ save it as a dependency in the package.json file.
 
 Press ^C at any time to quit.
 
-Installing packages using -g "globally":
+// Installing packages using -g "globally":
 npm install -g live-server
 
-Live Server creates a server to view html
+// Live Server creates a server to view html
 live-server
 
-Installing packages locally:
+// Installing packages locally:
 npm install lodash 
 
 ****INCORRECT*****
@@ -36,10 +36,22 @@ import { without } from 'lodash';
 console.log(without); 
 //error is displayed unknown token 
 
-install package browserify:
+// install package browserify:
 npm install -g browserify
 
 ****CORRECT*****
-After browserify install type in browserify script.js > bundle.js
+After browserify install type in
+browserify script.js > bundle.js
+
 bundle.js is created with all dependencies (lodash), then changed in html script > bundle.
+
+// Console error: "Unchecked runtime.lastError: The message port closed before a response was received."
+check if ad blocker or any extensions is causing this try disabling ad blocker/vid plugins
+
+// Every change in script.js needs to be updated in Gitbash run:
+browserify script.js > bundle.js
+
+// If node_modules is deleted as 
+// long as there is a package.json file it can be restorted by:
+npm install
 

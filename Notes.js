fa6ba0c6a6af19e767e2230af758c3cf6d397f90
dee <57253652+Angelus81@users.noreply.github.com>
2019-11-27 18:21:00 +0200

    changed script to test & css to test

---
+++ Notes.js
@@ -0,0 +1,25 @@
+To check version of Node:
+npm -v
+5.5.1
+
+node -v
+v12.13.1
+
+To upgrade, run: 
+npm install npm@latest -g
+
+Creates package.json file:
+npm init
+This utility will walk you through creating a package.json file.
+It only covers the most common items, and tries to guess sensible defaults.
+
+See `npm help json` for definitive documentation on these fields
+and exactly what they do.
+
+Use `npm install <pkg>` afterwards to install a package and
+save it as a dependency in the package.json file.
+
+Press ^C at any time to quit.
+
+Installing packages using -g "globally"
+npm install -g live-server
